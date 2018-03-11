9225f3a4d29d17bc7544806358c8bf4ce6ebbbee
sid <es15btech11018@iith.ac.in>
2018-03-11 20:23:37 +0530

    second commit

--- Assgn4-newbarr-ES15BTECH11018.cpp
+++ Assgn4-newbarr-ES15BTECH11018.cpp
@@ -19,9 +19,9 @@ uniform_int_distribution<int> dist_post(10000, 20000);
 ifstream in("inp-params.txt");
 FILE *fp = fopen("new-barr-log.txt", "w");
 FILE *fpp = fopen("pthread-barr-log.txt", "w");
-FILE * fa = fopen("Average_time.txt", "a+");
+FILE * fa = fopen("Average_time.txt", "w");
 
-double average_time = 0.0;
+double average_time = 0.0, paverage_time = 0.0;
 
 //get formatted time
 string getTime(time_t input)
@@ -34,20 +34,41 @@ string getTime(time_t input)
   return tim;
 }
 
+sem_t sem_counter;
+sem_t sem_barrier;
+int counter = 0;
+pthread_barrier_t barrier;
+
 void* worker(void* thr_id) {
     int tid = *(int *) thr_id;
     for(int i = 0; i < k; i++) {
+
         time_t beforeBarrSleep = time(NULL);
         string reqTime = getTime(beforeBarrSleep);
         
         fprintf(fp, "Going to sleep before the %dth barrier invocation by Thread %d at time : %s\n", i + 1, tid, reqTime.c_str());
         usleep(dist_pre(gen1));
         
-        //read
         time_t entry_time = time(NULL);
         string entryTime = getTime(entry_time);
         fprintf(fp, "Before the %dth barrier invocation by Thread %d at time : %s\n", i + 1, tid, entryTime.c_str());
         //barrier point
+	
+	sem_wait(&sem_counter); //wait on counter 
+	if(counter == n - 1) { //If last thread to enter
+	   counter = 0;
+	   sem_post(&sem_counter); //update counter to 0 and free it
+
+	   for(int i = 0; i < n - 1; i++) { //post every thread waiting on barrier
+	   	sem_post(&sem_barrier);
+	   }
+	}	
+	else {
+	   counter++; //update and free counter
+	   sem_post(&sem_counter); 
+
+	   sem_wait(&sem_barrier); //wait on barrier
+	}
 
         time_t after_barrier = time(NULL);
         string afterTime = getTime(after_barrier);
@@ -56,29 +77,86 @@ void* worker(void* thr_id) {
 
         time_t exit_time = time(NULL);
         string exitTime = getTime(exit_time);
-        fprintf(fp, "%dth CS Exit by Reader Thread %d at %s\n", i + 1, tid, exitTime.c_str());
-        average_time += (entry_time - beforeBarrSleep); 
+        
+        fprintf(fp, "Going to sleep after the %dth barrier invocation by Thread %d at time : %s\n", i + 1, tid, exitTime.c_str());
+        average_time += (exit_time - entry_time); 
+        usleep(dist_post(gen2));
+    }
+}
+
+
+void* pworker(void* thr_id) {
+    int tid = *(int *) thr_id;
+    for(int i = 0; i < k; i++) { 
+
+        time_t beforeBarrSleep = time(NULL);
+        string reqTime = getTime(beforeBarrSleep);
+        
+        fprintf(fpp, "Going to sleep before the %dth barrier invocation by Thread %d at time : %s\n", i + 1, tid, reqTime.c_str());
+        usleep(dist_pre(gen1));
+        
+        time_t entry_time = time(NULL);
+        string entryTime = getTime(entry_time);
+        fprintf(fpp, "Before the %dth barrier invocation by Thread %d at time : %s\n", i + 1, tid, entryTime.c_str());
+        //barrier point
+	pthread_barrier_wait(&barrier);
+
+        time_t after_barrier = time(NULL);
+        string afterTime = getTime(after_barrier);
+        fprintf(fpp, "After the %dth barrier invocation by Thread %d at time : %s\n", i + 1, tid, afterTime.c_str());
+
+
+        time_t exit_time = time(NULL);
+        string exitTime = getTime(exit_time);
+        
+        fprintf(fpp, "Going to sleep after the %dth barrier invocation by Thread %d at time : %s\n", i + 1, tid, exitTime.c_str());
+        paverage_time += (exit_time - entry_time); 
         usleep(dist_post(gen2));
     }
 }
 
 int main() {
     in >> n >> k >> prel >> postl;
-    pthread_t worker_threads[n];
-
+    pthread_t worker_threads[n], pworker_threads[n];
+    int ids[n];	
     gen1.seed(prel); //Assign respective seeds to the random generators
     gen2.seed(postl);
+    
+    sem_init(&sem_counter, 0, 1);
+    sem_init(&sem_barrier, 0, 0);
 
     for(int i = 0; i < n; i++) { //Call threads
-        pthread_create(&worker_threads[i], NULL, worker, &i);
+        ids[i] = i;
+	pthread_create(&worker_threads[i], NULL, worker, &ids[i]);
     }
 
     for(int i = 0; i < n; i++) {
         pthread_join(worker_threads[i], NULL);
     }
 
+    
+
     average_time /= n;
     
-    fprintf(fa, "Average time to enter CS : %f\n", average_time);
+    fprintf(fa, "Average time to cross barrier semaphore : %f\n", average_time);
+    
+    pthread_barrier_init(&barrier, NULL, n);
+
+    for(int i = 0; i < n; i++) { //Call threads
+        ids[i] = i;
+	pthread_create(&pworker_threads[i], NULL, pworker, &ids[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        pthread_join(pworker_threads[i], NULL);
+    }
+
+    
+    paverage_time /= n;
+    
+    fprintf(fa, "Average time to cross barrier pthread : %f\n", paverage_time);
+       
+    
+    
     return 0;
 }
